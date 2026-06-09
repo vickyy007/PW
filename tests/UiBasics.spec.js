06aeeebdaf96e83fec2009f9604f848a2c104f25
vikhyath <vicky.vikhyath.932@gmail.com>
2026-06-09 14:51:02 +0530

    Updated automation scripts

--- tests/UiBasics.spec.js
+++ tests/UiBasics.spec.js
@@ -42,11 +42,14 @@
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     const userName = page.locator('#username');
     const signIn = page.locator("#signInBtn");
+
     const documentLink = page.locator("[href*='documents-request']");
+
     const dropdown = page.locator("select.form-control");
     await dropdown.selectOption("consult");
     await page.locator(".radiotextsty").last().click();
     await page.locator("#okayBtn").click();
+    
     console.log(await page.locator(".radiotextsty").last().isChecked());
     await expect(page.locator(".radiotextsty").last()).toBeChecked();
     await page.locator("#terms").click();
