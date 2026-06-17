24e04f242c28f361fa1797e1903fb740c7e173e2
vikhyath <vicky.vikhyath.932@gmail.com>
2026-06-17 12:23:50 +0530

    formatted

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -8,14 +8,17 @@ test('@Webst Client App login', async ({ page }) => {
    const email = "anshika@gmail.com";
    const productName = 'ZARA COAT 3';
    const products = page.locator(".card-body");
+
    await page.goto("https://rahulshettyacademy.com/client");
    await page.locator("#userEmail").fill(email);
    await page.locator("#userPassword").fill("Iamking@000");
    await page.locator("[value='Login']").click();
    await page.waitForLoadState('networkidle');
    await page.locator(".card-body b").first().waitFor();
+
    const titles = await page.locator(".card-body b").allTextContents();
    console.log(titles); 
+   
    const count = await products.count();
    for (let i = 0; i < count; ++i) {
       if (await products.nth(i).locator("b").textContent() === productName) {
